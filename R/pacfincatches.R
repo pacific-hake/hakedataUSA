770bcd69be6e394408559b643ef0495c627384d0
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-01-06 04:37:01 -0800

    document: remove summaryfile from pacfincatches
    Will move all the cat code to a test

--- R/pacfincatches.R
+++ R/pacfincatches.R
@@ -4,25 +4,16 @@
 pacfincatches <- function(pcatch = NULL) {
   
   mydir <- hakedatawd()
-  summaryfile <- file.path(mydir, "extractedData", 
-    paste0("summary_catchPacFIN_", format(Sys.time(), "%Y.%m.%d"), ".txt"))
-  on.exit(suppressWarnings(sink(file = NULL)), add = TRUE)
-  sink(summaryfile)
-  cat("Summary of PacFIN catches created on", 
-    format(Sys.time(), "%Y-%m-%d %H:%M:%S"), "\n\n")
-  sink()
 
   if (is.null(pcatch)) {
     base::load(file.path(mydir, "extractedData", "Pacfincomp_ft_taylorCatch.Rdat"))
   }
   
-  sink(summaryfile, append = TRUE)
   cat("Summary of PacFIN catches by year and fleet.\n")
   print(tapply(pcatch$MT,list(pcatch$YEAR, pcatch$FLEET),sum))
   cat("\n\nThe following XX catches (mt) were removed.\n")
   xxcatch <- pcatch[pcatch$FLEET == "XX", ]
   print(tapply(xxcatch$MT, xxcatch$YEAR, sum))
-  sink()
   #todo: determine if these catches are foreign?
   # are they already accounted for in Canada?
   pcatch <- pcatch[pcatch$FLEET != "XX",]
@@ -49,10 +40,8 @@ pacfincatches <- function(pcatch = NULL) {
   tribal.yr.per <- setNames(
     aggregate(tribal$MT,list(tribal$Month,tribal$YEAR),sum),
     c("Period","Year","MT"))
-  sink(summaryfile, append = TRUE)
   cat("\nSummary of tribal catch by year.\n")
   print(stats::aggregate(MT ~ YEAR, data = tribal, sum))
-  sink()
 
   invisible(list("PacFINTribal" = tribal, "Foreign" = xxcatch,
     "PacFINbySMY" = pcatch.yr.per))
