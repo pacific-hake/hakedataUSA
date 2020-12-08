076337d8aa13f600583460eaf14dd65c4c405c2e
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-12-08 05:47:29 -0800

    fix(UScatchPlots): squish together and remove x axis from upper plots
    * repeated axes were a waste, now x-axis is only on bottom plots
    * add argument to allow for the removal of (preliminary) from the header

--- R/UScatchPlots.R
+++ R/UScatchPlots.R
@@ -4,18 +4,27 @@
 #' todo: Make CP and MS use NORPAC data instead of PacFIN.
 #' 
 #' @param doPNG A logical specifying if png files should be saved.
-#' @param nyears The number of years you want to plot.
+#' @param nyears The number of years you want to plot. The maximum year
+#' will be found and \code{nyears} will be sequenced into history until
+#' the desired range is found. Years will be included in this range even
+#' if they do not have any data.
+#' @param preliminary A logical value specifying if the data represents
+#' preliminary data available prior to the first week of January when
+#' the final data pull is normally extracted from the data warehouses.
+#' The default, of \code{TRUE}, adds \code{"(preliminary)"} to the title
+#' of each plot.
 #' @export
 #' @author Kelli Faye Johnson
 #'
 #' @return todo: document return of UScatchPlots
 #' 
-UScatchPlots <- function(doPNG = TRUE, nyears = 5) {
+UScatchPlots <- function(doPNG = TRUE, nyears = 5, preliminary = TRUE) {
   mydir <- hakedatawd()
   args <- list(height = 5, width = 10, units = "in", pointsize = 10, res = 300)
-  args2 <- list(mfrow=c(2,2),mar=c(4,4.1,0.5,0.5),oma=c(0,0.1,2,0.1),
+  args2 <- list(mfrow=c(2,2),mar=c(0.5,4.1,0.5,0.5),oma=c(4,0.1,2,0.1),
     mgp=c(2.1, 0.75, 0), las = 1)
   args3 <- list(outer=TRUE,side=3,line=-0.3,cex=1.3)
+  args4 <- list(outer=TRUE,side=1,line=1.5,cex=1.3)
 
   quotas <- read.csv(file.path(mydir, "Catches", "quotas.csv"), 
     sep = ",", header = TRUE, check.names = FALSE)
@@ -40,7 +49,12 @@ UScatchPlots <- function(doPNG = TRUE, nyears = 5) {
   plotHakeCatchMonthYear.fn(shore[shore$sector=="USshore",],
     Yrs=Yrs,quotas=quotas[3,],lineWds=lineWds,lineTypes=lineTypes,
     cols=cols,leg.cex=0.7)
-  do.call("mtext", c(args3, text = "U.S. Shoreside Catches (preliminary)"))
+  do.call("mtext", c(args3,
+    text = paste(
+      "U.S. Shoreside Catches",
+      ifelse(preliminary, " (preliminary)", "")))
+  )
+  do.call("mtext", c(args4, text = "Month"))
   if (doPNG) dev.off()
 
   if (doPNG) {
@@ -51,7 +65,12 @@ UScatchPlots <- function(doPNG = TRUE, nyears = 5) {
   plotHakeCatchMonthYear.fn(cp,
     Yrs=Yrs,quotas=quotas[1,-1],lineWds=lineWds,lineTypes=lineTypes,
     cols=cols,leg.cex=0.7)
-  do.call("mtext", c(args3, text = "U.S. CP Catches (preliminary)"))
+  do.call("mtext", c(args3,
+    text = paste(
+      "U.S. CP Catches",
+      ifelse(preliminary, " (preliminary)", "")))
+  )
+  do.call("mtext", c(args4, text = "Month"))
   if(doPNG) dev.off()  
 
   if (doPNG) {
@@ -62,7 +81,12 @@ UScatchPlots <- function(doPNG = TRUE, nyears = 5) {
   plotHakeCatchMonthYear.fn(ms,
     Yrs=Yrs,quotas=quotas[2,-1],lineWds=lineWds,lineTypes=lineTypes,
     cols=cols,leg.cex=0.7)
-  do.call("mtext", c(args3, text = "U.S. MS Catches (preliminary)"))
+  do.call("mtext", c(args3,
+    text = paste(
+      "U.S. MS Catches",
+      ifelse(preliminary, " (preliminary)", "")))
+  )
+  do.call("mtext", c(args4, text = "Month"))
   if(doPNG) dev.off()  
 
   tmp <- tapply(cp$catch,cp$year,sum)["2017"]
