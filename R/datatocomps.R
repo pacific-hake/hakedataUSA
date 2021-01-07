b8a2a94b8021b8655bfdb736d3d843cf47bcd90e
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-01-06 21:29:10 -0800

    fix(datatocomps): CAN keeps changing how comps are saved, this will accommodate any way

--- R/datatocomps.R
+++ R/datatocomps.R
@@ -3,22 +3,47 @@ datatocomps <- function(dirdata, dirmod, cohorts) {
   options(default.stringsAsFactors = FALSE)
   options(stringsAsFactors = FALSE)
 
-  can.l <- lapply(
-    dir(dirdata, "canada.*-age", full.names = TRUE),
-    function(x) {
-      out <- read.csv(x, header = TRUE, check.names = FALSE)
-      out$Sector <- gsub(".*canada-([-a-z]*)-age.*", "\\1", x)
-      out$Sector[out$Sector == "freezer-trawler"] <- "CAN_FreezeTrawl"
-      out$Sector[out$Sector == "joint-venture"] <- "CAN_JV"
-      out$Sector[out$Sector == "shoreside"] <- "CAN_Shoreside"
-      colnames(out) <- gsub("^([1-9]{1})", "a\\1", colnames(out))
-      colnames(out) <- gsub("^N$", "Nsamples", colnames(out))
-      colnames(out)[1] <- "Year"
-      out <- out[, c("Sector", "Year", 
-        grep("a[0-9]", colnames(out), value = TRUE), "Nsamples")]
-      return(out)
-    })
-  can.l <- do.call(rbind, can.l)
+  canfile <- file.path(dirdata, "can-age-data.csv")
+  if (file.exists(canfile)) {
+    can <- readLines(canfile)
+    cansplit <- strsplit(can, ",")
+    cansplit <- lapply(cansplit, function(x){c(x[1],x[-1][!x[-1] == ""])})
+    canlabel <- cansplit[which(sapply(cansplit, length) == 1)]
+    canlabel <- gsub(".+Trawler.+", "CAN FreezerTrawl", canlabel)
+    canlabel <- gsub(".+side.+", "CAN Shoreside", canlabel)
+    canlabel <- gsub(".+Venture.+", "CAN JV", canlabel)
+    temp <- which(sapply(cansplit, length) == 2)
+    ncan <- data.frame(rep(canlabel[4:6], 
+      diff(c(0, 
+        which(!diff(temp) == 1), length(temp)))), 
+        do.call("rbind", cansplit[temp]))
+    colnames(ncan) <- c("Sector", "Year", "Nsamples")
+    temp <- which(sapply(cansplit, length) > 2)
+    cancomp <- data.frame(rep(canlabel[1:3], 
+      diff(c(1, 
+        which(!diff(temp) == 1), length(temp)))), 
+        do.call("rbind", cansplit[temp[-1]]))
+    colnames(cancomp) <- c("Sector", "Year", paste0("a", 3:ncol(cancomp) - 2))
+    can.l <- merge(cancomp, ncan, all.x = TRUE)
+  } else {
+    can.l <- lapply(
+      dir(dirdata, "can[ada]*.*-age", full.names = TRUE),
+      function(x) {
+        out <- read.csv(x, header = TRUE, check.names = FALSE)
+        out$Sector <- gsub(".*canada-([-a-z]*)-age.*", "\\1", x)
+        out$Sector[out$Sector == "freezer-trawler"] <- "CAN_FreezeTrawl"
+        out$Sector[out$Sector == "joint-venture"] <- "CAN_JV"
+        out$Sector[out$Sector == "shoreside"] <- "CAN_Shoreside"
+        colnames(out) <- gsub("^([1-9]{1})", "a\\1", colnames(out))
+        colnames(out) <- gsub("^N$", "Nsamples", colnames(out))
+        colnames(out)[1] <- "Year"
+        out <- out[, c("Sector", "Year", 
+          grep("a[0-9]", colnames(out), value = TRUE), "Nsamples")]
+        return(out)
+      })
+    can.l <- do.call(rbind, can.l)
+  }
+
   usc <- utils::read.csv(file = file.path(dirdata, "us-cp-age-data.csv"),
     stringsAsFactors = FALSE)
   usm <- utils::read.csv(file = file.path(dirdata, "us-ms-age-data.csv"),
@@ -35,7 +60,6 @@ datatocomps <- function(dirdata, dirmod, cohorts) {
   comps <- merge(can.l, uscomp, all = TRUE)
   
   catch <- utils::read.csv(file.path(dirdata, "landings-tac-history.csv"))
-  
   temp <- c(
     "atSea_US_MS", "atSea_US_CP", "US_shore", 
     "CAN_JV", "CAN_Shoreside", "CAN_FreezeTrawl")
@@ -98,30 +122,4 @@ datatocomps <- function(dirdata, dirmod, cohorts) {
   r4ss::SS_writedat(ssdat, file.path(dirmod, "hake_data.ss"),
     overwrite = TRUE, verbose = FALSE)
   return(final)
-}
-
-datatoassessment_comp <- function(dirassessment) {
-  dircomp = file.path(hakedatawd(), "Catches", "Comps")
-  dirsave <- file.path(dirassessment, "data")
-  aa <- utils::read.csv(file.path(dircomp, "CP.Age.Only", "comps.csv"))
-  colnames(aa)[1:3] <- c("year", "n.fish", "n.hauls")
-  colnames(aa) <- gsub("Age", "a", colnames(aa))
-  aa[is.na(aa)] <- 0
-  utils::write.table(x = aa, 
-    file = file.path(dirsave, "us-cp-age-data.csv"), sep = ",",
-    row.names = FALSE)
-  aa <- utils::read.csv(file.path(dircomp, "MS.Age.Only", "comps.csv"))
-  colnames(aa)[1:3] <- c("year", "n.fish", "n.hauls")
-  colnames(aa) <- gsub("Age", "a", colnames(aa))
-  aa[is.na(aa)] <- 0
-  utils::write.table(x = aa, 
-    file = file.path(dirsave, "us-ms-age-data.csv"), sep = ",",
-    row.names = FALSE)
-  aa <- utils::read.csv(file.path(dircomp, "Shoreside.Age.Only", "shoresideAgeComps.csv"))
-  colnames(aa)[1:2] <- c("year", "n.trips")
-  colnames(aa) <- gsub("Age", "a", colnames(aa))
-  aa[is.na(aa)] <- 0
-  utils::write.table(x = aa, 
-    file = file.path(dirsave, "us-shore-age-data.csv"), sep = ",",
-    row.names = FALSE)
 }
