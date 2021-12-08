568e515c8e0443714b5a32ef0e2fe0acba021847
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-12-08 15:05:46 -0800

    lintr(comps_plot)

--- R/comps_plot.R
+++ R/comps_plot.R
@@ -3,8 +3,12 @@
 comps_plot <- function() {
   mydir <- hakedatawd()
   datfile <- file.path(mydir, "Catches", "Comps", "AgeCompsForSS.txt")
-  if (!file.exists(datfile)) stop("The file, ", datfile, " was not found.\n",
-    "Must combine composition data for SS to run comps_plot.")
+  if (!file.exists(datfile)) {
+    stop(
+      "The file, ", datfile, " was not found.\n",
+      "Must combine composition data for SS to run comps_plot."
+    )
+  }
   dat <- read.table(file = datfile)
   yrs <- dat$V1
   comps <- dat[, paste0("V", 10:ncol(dat))]
@@ -14,20 +18,24 @@ comps_plot <- function() {
 
   for (minyear in c(min(yrs), max(yrs) - 15)) {
     png(
-      filename = file.path(mydir, "Catches", "Comps", 
-        paste0("propnatage_fish_minyear", minyear, ".png")),
-      height=6, width=15, units = "in", res = 600)
+      filename = file.path(
+        mydir, "Catches", "Comps",
+        paste0("propnatage_fish_minyear", minyear, ".png")
+      ),
+      height = 6, width = 15, units = "in", res = 600
+    )
     par(mfrow = c(1, 1), mar = c(4, 4, 2, 1))
-    symbols(x, y, circles = z,
+    symbols(x, y,
+      circles = z,
       inches = 0.2, xlim = range(yrs[yrs >= minyear]),
       xaxt = "n", yaxt = "n", ylim = c(1, ncol(comps)),
-      bg = rgb(0,0,1,0.6), 
+      bg = rgb(0, 0, 1, 0.6),
       xlab = "Year", ylab = "Age",
-      las = 1)
-    axis(1,at=yrs)
-    axis(2,at=1:ncol(comps),las=1)
-    title(main="Proportion of numbers-at-age (coastwide fishery)")
+      las = 1
+    )
+    axis(1, at = yrs)
+    axis(2, at = 1:ncol(comps), las = 1)
+    title(main = "Proportion of numbers-at-age (coastwide fishery)")
     dev.off()
   }
-
 }
